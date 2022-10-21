23bab959fe8bc7685d6c9d5e496d07e68626a8b7
Юля Кобзева <selemina@mail.ru>
2022-10-21 15:32:13 +0300

    ДЗ2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Main {
@@ -9,7 +11,11 @@ public class Main {
         Product персик = new Product("персик", 110.0, 1.0);
         Product виноград = new Product("виноград", 150.0, 1.0);
         Product киви = new Product("киви", 90.0, 1.0);
-
+        Product яйца = new Product("яйца", 100.0, 1.0);
+        Product мука = new Product("мука", 80.0, 1.0);
+        Product масло = new Product("масло", 120.0, 1.0);
+        Product молоко = new Product("молоко", 100.0, 1.0);
+        Product йогурт = new Product("йогурт", 90.0, 1.0);
 
         Set<Product> listOfProducts2 = new HashSet<>();
         ListOfProducts listOfProducts = new ListOfProducts(listOfProducts2);
@@ -23,8 +29,23 @@ public class Main {
         listOfProducts.remove(персик);
         listOfProducts.isBought();
 
-        listOfProducts.add(киви);
+//        listOfProducts.add(киви);
+
+        List<Product> blinyProducts = List.of(яйца, мука, молоко);
+        Recipe bliny = new Recipe(blinyProducts,"блины");
+        System.out.println(bliny);
+
+        List<Product> keksProducts = List.of(яйца, мука, масло, йогурт);
+        Recipe keks = new Recipe(keksProducts,"кекс");
+        System.out.println(keks);
+
+        Set<Recipe> bookOfRecipes2 = new HashSet<>();
+        BookOfRecipes bookOfRecipes = new BookOfRecipes(bookOfRecipes2);
+
+        bookOfRecipes.add(bliny);
+        bookOfRecipes.add(keks);
+//        bookOfRecipes.add(keks);
+
+        System.out.println(bookOfRecipes);
     }
-
-
 }
