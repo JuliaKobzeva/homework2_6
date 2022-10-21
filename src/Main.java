0c2af03ffd46ead77dc18bdc8197adaed212f676
Юля Кобзева <selemina@mail.ru>
2022-10-21 19:40:39 +0300

    ДЗ2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        Random random = new Random();
+
+        Integer number = random.nextInt(1000);
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            list.add(number);
+        }
+
+        System.out.println(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            Integer element = list.get(i);
+            if(element  % 2 != 0){
+                list.remove(element);
+            }
+        }
+
+        System.out.println(list);
     }
 }
