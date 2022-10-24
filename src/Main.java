a9839923e24950868547537a6b7ad67a083db24c
Юля Кобзева <selemina@mail.ru>
2022-10-25 01:51:52 +0300

    ДЗ3

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,21 @@ public class Main {
 
         Random random = new Random();
 
-        Set<String> list = new HashSet<>(15);
+        Set<String> list = new HashSet<>();
 
-        for (int i = 0; i < 15; i++) {
+        List<String> list2 = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
             int a = random.nextInt(4)+2;
             int b = random.nextInt(5)+5;
             String task = a + "*" + b;
             list.add(task);
-            System.out.println(task);
+        }
+
+        list2.addAll(list);
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println(list2.get(i));
         }
 
     }
