d5468db6139b266a8a3bac3dcf448f7880e67d3a
Юля Кобзева <selemina@mail.ru>
2022-10-29 00:09:43 +0300

    ДЗ2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
 
-        Map<String, List<Integer>> map = new HashMap<>();
+        LinkedHashMap<Integer,String> map = new LinkedHashMap<>();
 
         Integer number1 = random.nextInt(1000);
         Integer number2 = random.nextInt(1000);
@@ -19,45 +16,21 @@ public class Main {
         Integer number8 = random.nextInt(1000);
         Integer number9 = random.nextInt(1000);
         Integer number10 = random.nextInt(1000);
-        Integer number11 = random.nextInt(1000);
-        Integer number12 = random.nextInt(1000);
-        Integer number13 = random.nextInt(1000);
-        Integer number14 = random.nextInt(1000);
-        Integer number15 = random.nextInt(1000);
 
+        map.put(number1,"Число №1");
+        map.put(number2,"Число №2");
+        map.put(number3,"Число №3");
+        map.put(number4,"Число №4");
+        map.put(number5,"Число №5");
+        map.put(number6,"Число №6");
+        map.put(number7,"Число №7");
+        map.put(number8,"Число №8");
+        map.put(number9,"Число №9");
+        map.put(number10,"Число №10");
 
-        List<Integer>list = List.of(number1,number2,number3);
-        List<Integer>list2 = List.of(number4,number5,number6);
-        List<Integer>list3 = List.of(number7,number8,number9);
-        List<Integer>list4 = List.of(number10,number11,number12);
-        List<Integer>list5 = List.of(number13,number14,number15);
-
-        map.put("Список №1",list);
-        map.put("Список №2",list2);
-        map.put("Список №3",list3);
-        map.put("Список №4",list4);
-        map.put("Список №5",list5);
-
-        for(Map.Entry<String, List<Integer>> element : map.entrySet()){
+        for(Map.Entry<Integer,String> element : map.entrySet()){
             System.out.println(element.getKey() + " " + element.getValue());
         }
 
-        Integer numberSum1 = number1 + number2 + number3;
-        Integer numberSum2 = number4 + number5 + number6;
-        Integer numberSum3 = number7 + number8 + number9;
-        Integer numberSum4 = number10 + number11 + number12;
-        Integer numberSum5 = number13 + number14 + number15;
-
-        Map<String, Integer> map2 = new HashMap<>();
-
-        map2.put("Список №1",numberSum1);
-        map2.put("Список №2",numberSum2);
-        map2.put("Список №3",numberSum3);
-        map2.put("Список №4",numberSum4);
-        map2.put("Список №5",numberSum5);
-
-        for(Map.Entry<String, Integer> element : map2.entrySet()){
-            System.out.println(element.getKey() + " " + element.getValue());
-        }
     }
 }
