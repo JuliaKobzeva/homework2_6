f764a212395c01ede48b10b2ddb71092edac2407
Юля Кобзева <selemina@mail.ru>
2022-10-29 23:33:29 +0300

    ДЗ2

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,15 @@ public class Main {
         List<String> list2 = new ArrayList<>();
 
         for (int i = 0; i < 30; i++) {
-            int a = random.nextInt(4)+2;
-            int b = random.nextInt(5)+5;
+//            int a = random.nextInt(4)+2;
+//            int b = random.nextInt(5)+5;
+            int a = random.nextInt(8)+2;
+            int b = random.nextInt(8)+2;
             String task = a + "*" + b;
             list.add(task);
+            if(list.contains(task) && list.contains(reverseString(task))){
+                list.remove(task);
+            }
         }
 
         list2.addAll(list);
@@ -22,5 +27,9 @@ public class Main {
             System.out.println(list2.get(i));
         }
 
+    }
+
+    public static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 }
